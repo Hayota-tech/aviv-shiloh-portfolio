94aae558dc5d38fcb21a562585b2f7e8af92c4fc
amir-ben-shimol <abamirbs11@gmail.com>
2023-04-06 18:36:08 +0300

    add mobile header

---
+++ src/assets/icons.ts
@@ -0,0 +1,10 @@
+const icons = {
+	humburger: [
+		'24 24',
+		`
+		<path d="M2 12H22.5M2 22.25H22.5M2 1.75H22.5" stroke="white" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+		`,
+	],
+};
+
+export default icons;
--- src/components/layout/Header/Header.view.tsx
+++ src/components/layout/Header/Header.view.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 
 import classes from './Header.module.scss';
 import { concatClasses } from '@/utils/component';
+import SSvg from '@/ui/SSvg';
 
 interface IProps {
 	readonly theme?: string;
@@ -12,10 +13,16 @@ interface IProps {
 const HeaderView: React.FC<IProps> = (props: React.PropsWithChildren<IProps>) => {
 	const containerClass = concatClasses(classes, 'container', props.float ? 'container--float' : '');
 
-	const linkClass = concatClasses(
+	const menuClass = concatClasses(
 		classes,
-		'container__link',
-		props.theme === 'dark' ? 'container__link--dark' : 'container__link--light',
+		'container__menu',
+		props.theme === 'dark' ? 'container__menu--dark' : 'container__menu--light',
+	);
+
+	const logoClass = concatClasses(
+		classes,
+		'container__logo',
+		props.theme === 'dark' ? 'container__logo--dark' : 'container__logo--light',
 	);
 
 	const textClass = concatClasses(
@@ -24,12 +31,23 @@ const HeaderView: React.FC<IProps> = (props: React.PropsWithChildren<IProps>) =>
 		props.theme === 'dark' ? 'container__text--dark' : 'container__text--light',
 	);
 
+	const mobileBurgerClass = concatClasses(
+		classes,
+		'mobileBurger__icon',
+		props.theme === 'dark' ? 'mobileBurger__icon--dark' : 'mobileBurger__icon--light',
+	);
+
 	return (
 		<header className={containerClass}>
-			<Link className={linkClass} href="/about">
+			<div className={classes['mobileBurger']}>
+				<button className={classes['mobileBurger__button']} type="button">
+					<SSvg name="humburger" className={mobileBurgerClass} />
+				</button>
+			</div>
+			<Link className={menuClass} href="/about">
 				MENU
 			</Link>
-			<Link className={linkClass} href="/">
+			<Link className={logoClass} href="/">
 				Aviv Shiloh
 			</Link>
 			<span className={textClass}>©2023</span>
