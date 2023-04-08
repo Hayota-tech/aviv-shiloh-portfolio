c72be45210d65b04c3ac58dcba767154a0ba2244
amir-ben-shimol <abamirbs11@gmail.com>
2023-04-08 20:09:33 +0300

    fixed small ui issues

--- src/components/containers/Contact/Contact.view.tsx
+++ src/components/containers/Contact/Contact.view.tsx
@@ -6,49 +6,51 @@ import { address } from '../../../data/address';
 import classes from './Contact.module.scss';
 
 interface IProps {
-	readonly onSocialNavigate: (_: string) => void;
+	readonly onSocialNavigate: (url: string) => void;
 }
 
 const ContactView: React.FC<IProps> = (props: React.PropsWithChildren<IProps>) => {
 	return (
 		<section className={classes['container']}>
-			<span className={classes['container__title']}>CONTACT</span>
 			<div className={classes['innerContainer']}>
-				<div className={classes['leftContainer']}>
-					<span className={classes['leftContainer__title']}>WHERE</span>
-					{address.map((element, i) => (
-						<span key={i} className={classes['leftContainer__adressText']}>
-							{element.name}
-						</span>
-					))}
-					<div className={classes['mailDesktop']}>
-						<span className={classes['mailDesktop__title']}>MAIL</span>
-						<a className={classes['mailDesktop__mail']} href="mailto:aviv.shiloh@gmail.com">
-							aviv.shiloh@gmail.com
-						</a>
+				<h3 className={classes['innerContainer__title']}>CONTACT</h3>
+				<div className={classes['contactContainer']}>
+					<div className={classes['leftSide']}>
+						<span className={classes['leftSide__title']}>WHERE</span>
+						{address.map((element, i) => (
+							<span key={i} className={classes['leftSide__adressText']}>
+								{element.name}
+							</span>
+						))}
+						<div className={classes['mailWeb']}>
+							<span className={classes['mailWeb__title']}>MAIL</span>
+							<a className={classes['mailWeb__mail']} href="mailto:aviv.shiloh@gmail.com">
+								aviv.shiloh@gmail.com
+							</a>
+						</div>
+					</div>
+					<div className={classes['rightSide']}>
+						<span className={classes['rightSide__title']}>SOCIAL</span>
+						{socialMedia.map((element, i) => (
+							<button
+								key={i}
+								className={classes['rightSide__socialButton']}
+								type="button"
+								onClick={() => props.onSocialNavigate(element.url)}
+							>
+								{element.name}
+							</button>
+						))}
 					</div>
 				</div>
-				<div className={classes['rightContainer']}>
-					<span className={classes['rightContainer__title']}>SOCIAL</span>
-					{socialMedia.map((element, i) => (
-						<button
-							key={i}
-							className={classes['rightContainer__socialButton']}
-							type="button"
-							onClick={() => props.onSocialNavigate(element.url)}
-						>
-							{element.name}
-						</button>
-					))}
-				</div>
-			</div>
-			<div className={classes['mailMobile']}>
-				<span className={classes['mailMobile__title']}>MAIL</span>
-				<a className={classes['mailMobile__mail']} href="mailto:aviv.shiloh@gmail.com">
-					aviv.shiloh@gmail.com
-				</a>
+				<div className={classes['mailMobile']}>
+					<span className={classes['mailMobile__title']}>MAIL</span>
+					<a className={classes['mailMobile__mail']} href="mailto:aviv.shiloh@gmail.com">
+						aviv.shiloh@gmail.com
+					</a>
 
-				<span className={classes['mobileSignature']}>©2023 AVIV SHILOH</span>
+					<span className={classes['mobileSignature']}>©2023 AVIV SHILOH</span>
+				</div>
 			</div>
 		</section>
 	);
