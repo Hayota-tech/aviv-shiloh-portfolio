c5531257997c82877d795837c45f545f6dad768c
amir-ben-shimol <abamirbs11@gmail.com>
2023-04-10 12:38:24 +0300

    add carousel

--- src/components/containers/Project/Project.view.tsx
+++ src/components/containers/Project/Project.view.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
-
-import { imageUrl } from '@/utils/image-url';
 
 import classes from './Project.module.scss';
 import { IProject } from 'src/interfaces/responses';
 import SSvg from '@/ui/SSvg';
+import ProjectCarousel from './ProjectCarousel';
 
 interface IProps {
 	readonly projectInfo: IProject | undefined;
@@ -20,9 +18,12 @@ const ProjectView: React.FC<IProps> = (props: React.PropsWithChildren<IProps>) =
 	return (
 		<section className={classes['container']}>
 			<div className={classes['infoContainer']}>
-				<Link className={classes['infoContainer__backButton']} href="/">
-					Back
-				</Link>
+				<div className={classes['backButtonContainer']}>
+					<SSvg className={classes['backButtonContainer__icon']} name="arrowLeft" />
+					<Link className={classes['backButtonContainer__text']} href={'/'}>
+						Back
+					</Link>
+				</div>
 				<h2 className={classes['infoContainer__name']}>{projectName}</h2>
 				<span className={classes['infoContainer__date']}>{projectDate}</span>
 				<p className={classes['infoContainer__description']}>{projectDescription}</p>
@@ -32,23 +33,7 @@ const ProjectView: React.FC<IProps> = (props: React.PropsWithChildren<IProps>) =
 				</div>
 			</div>
 			<div className={classes['imageSlideContainer']}>
-				{props.projectInfo?.attributes?.media?.data.map((image, i) => {
-					const imageSrc = imageUrl(image.attributes.url);
-					return (
-						<div key={i} className={classes['imageContainer']}>
-							<Image
-								className={classes['imageContainer__image']}
-								src={imageSrc}
-								alt={image.attributes.caption}
-								width={1}
-								height={1}
-							/>
-							<span className={classes['imageContainer__caption']}>
-								{image.attributes.caption}
-							</span>
-						</div>
-					);
-				})}
+				<ProjectCarousel imagesList={props.projectInfo?.attributes?.media?.data ?? []} />
 			</div>
 		</section>
 	);
