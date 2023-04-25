2795528fc9f7d287470bca40a5d37f4e5e5d21b8
amir-ben-shimol <abamirbs11@gmail.com>
2023-04-25 15:54:26 +0300

    ci: 🚀 init lint-stage

---
+++ lint-staged.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+	// 'apps/backend/**/*.ts': [
+	// 	'pnpm --filter backend exec eslint -c ./.eslintrc.cjs --ignore-path ./.eslintignore --fix',
+	// 	() => 'tsc --noEmit',
+	// ],
+	// 'apps/cli-backend/**/*.ts': [
+	// 	'pnpm --filter cli-backend exec eslint -c ./.eslintrc.cjs --ignore-path ./.eslintignore --fix',
+	// 	() => 'tsc --noEmit',
+	// ],
+	// 'apps/frontend/**/*.ts': [
+	// 	'pnpm --filter frontend exec eslint -c ./.eslintrc.cjs --ignore-path ./.eslintignore --fix',
+	// 	() => 'tsc --noEmit',
+	// ],
+	// 'packages/common/**/*.ts': [
+	// 	'pnpm --filter common exec eslint -c ./.eslintrc.cjs --ignore-path ./.eslintignore --fix',
+	// 	() => 'tsc --noEmit',
+	// ],
+	// '!({apps,packages})**/*.{ts,js,cjs}': ['eslint -c ./.eslintrc.cjs --fix', () => 'tsc --noEmit'],
+	// 'apps/frontend/src/**/*.scss': 'stylelint --config ./apps/frontend/stylelint.config.cjs --fix',
+	// '**/*.{ts,js,cjs,json,yaml}': 'prettier --write',
+	// '**/*': 'inflint -c ./inflint.config.ts',
+};
