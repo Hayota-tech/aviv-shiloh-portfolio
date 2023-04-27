50bcc68523d1d2137e66ec53d883674b0bbb61ef
beargreenholtz <114157462+beargreenholtz@users.noreply.github.com>
2023-04-27 17:16:30 +0300

    Update _app.tsx

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -9,10 +9,10 @@ import React from 'react';
 import { AnimatePresence } from 'framer-motion';
 
 import '../styles/custom.scss';
-//import { useNextCssRemovalPrevention } from '@/utils/fix-css';
+import { useNextCssRemovalPrevention } from '@/utils/fix-css';
 
 function MyApp({ Component, pageProps, router }: AppProps) {
-	//useNextCssRemovalPrevention()
+	useNextCssRemovalPrevention()
 
 	return (
 		<>
