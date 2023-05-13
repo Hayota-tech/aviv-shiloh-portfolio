51c46646dd4c80f7e23a05c4672d298316c30d26
amir-ben-shimol <abamirbs11@gmail.com>
2023-05-13 19:09:50 +0300

    refactor: 💡 align backend with prettier configurations

--- apps/backend/src/api/intro/controllers/intro.ts
+++ apps/backend/src/api/intro/controllers/intro.ts
@@ -2,6 +2,6 @@
  * intro controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::intro.intro');
--- apps/backend/src/api/video-category/controllers/video-category.ts
+++ apps/backend/src/api/video-category/controllers/video-category.ts
@@ -2,6 +2,6 @@
  * video-category controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::video-category.video-category');
