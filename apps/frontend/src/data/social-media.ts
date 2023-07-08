cdf2922659b754400834547c7531779e6e0c9eea
beargreenholtz <beargreenholtz@gmail.com>
2023-07-08 20:42:01 +0300

    refactor: 💡 fix commit error

--- apps/frontend/src/data/social-media.ts
+++ apps/frontend/src/data/social-media.ts
@@ -1,7 +1,13 @@
-export const socialMedia = [
-	{ name: 'Instagram', url: 'https://google.com' },
-	{ name: 'Facebook', url: 'https://www.facebook.com/spring.production.il' },
-	{ name: 'Linkedin', url: 'https://www.linkedin.com/company/spring-productions/about/' },
-	{ name: 'Youtube', url: 'https://google.com' },
-	{ name: 'Vimeo', url: 'https://google.com' },
+import type icons from '@/assets/icons';
+
+interface ISocialMedia {
+	readonly name: keyof typeof icons;
+	readonly url: string;
+}
+
+export const socialMedia: ISocialMedia[] = [
+	{ name: 'instagram', url: 'https://www.instagram.com/aviv_shiloh' },
+	{ name: 'facebook', url: 'https://www.facebook.com/aviv.shiloh' },
+	{ name: 'linkedin', url: 'https://www.linkedin.com/in/aviv-shiloh-67a60974/' },
+	{ name: 'youtube', url: 'https://www.youtube.com/@avivshiloh8475' },
 ];
